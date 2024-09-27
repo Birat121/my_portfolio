c0ceff6b557a7e386244b95ad6548aeefc33aa76
Birat Budhathoki <biratbudhathoki79@gmail.com>
2024-09-27 21:13:17 +0545

    some changes

--- react-portfolio-main/src/components/Projects/ProjectCard.jsx
+++ react-portfolio-main/src/components/Projects/ProjectCard.jsx
@@ -24,13 +24,8 @@ export const ProjectCard = ({
           );
         })}
       </ul>
-      <div className={styles.links}>
-        
-        
-        <a href={source} className={styles.link}>
-          Source
-        </a>
-      </div>
+      
+      
     </div>
   );
 };
